1cfa233c6b705e8589ddc3488513edb09973e1b1
Anthony Sikaneta <sikaneta@gmail.com>
2022-02-18 07:26:15 +0200

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "tailwindcss/dist/base.css";
 import "styles/globalStyles.css";
 
-import ThreeColCenteredStatsPrimaryBackground from "components/features/ThreeColCenteredStatsPrimaryBackground";
-import TwoColWithButton from "components/features/TwoColWithButton";
 import React from "react";
-import { css } from "styled-components/macro"; //eslint-disable-line
 
-import Header from "./components/headers/light";
 import MiniCenteredFooter from "./components/footers/MiniCenteredFooter";
 import PortfolioTwoCardsWithImage from "components/cards/PortfolioTwoCardsWithImage";
 import BackgroundAsImage from "components/hero/BackgroundAsImage";
--- src/components/hero/BackgroundAsImage.js
+++ src/components/hero/BackgroundAsImage.js
@@ -51,7 +51,7 @@ const SlantedBackground = styled.span`
 
 const Notification = tw.span`inline-block my-4 pl-3 py-1 text-gray-100 border-l-4 border-blue-500 font-medium text-sm`;
 
-const PrimaryAction = tw.button`px-8 py-3 mt-10 text-sm sm:text-base sm:mt-16 sm:px-8 sm:py-4 bg-gray-100 text-primary-500 font-bold rounded shadow transition duration-300 hocus:bg-primary-500 hocus:text-gray-100 focus:shadow-outline`;
+// const PrimaryAction = tw.button`px-8 py-3 mt-10 text-sm sm:text-base sm:mt-16 sm:px-8 sm:py-4 bg-gray-100 text-primary-500 font-bold rounded shadow transition duration-300 hocus:bg-primary-500 hocus:text-gray-100 focus:shadow-outline`;
 
 const StyledResponsiveVideoEmbed = styled(ResponsiveVideoEmbed)`
   padding-bottom: 56.25% !important;

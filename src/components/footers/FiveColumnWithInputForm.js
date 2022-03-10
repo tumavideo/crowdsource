4e1af0ce28324f706e5795e4b99ea9cb3a4278ae
Anthony Sikaneta <sikaneta@gmail.com>
2022-03-10 06:55:18 +0200

    Remove unused components

--- src/components/footers/FiveColumnWithInputForm.js
+++ src/components/footers/FiveColumnWithInputForm.js
@@ -1,8 +1,6 @@
 import React from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
-import { css } from "styled-components/macro"; //eslint-disable-line
-import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
 
 import LogoImage from "images/logo.svg";
 import { ReactComponent as FacebookIcon } from "images/facebook-icon.svg";
@@ -20,17 +18,6 @@ const ColumnHeading = tw.h5`uppercase font-bold`;
 const LinkList = tw.ul`mt-6 text-sm font-medium`;
 const LinkListItem = tw.li`mt-3`;
 const Link = tw.a`border-b-2 border-transparent hocus:border-gray-700 pb-1 transition duration-300`;
-
-const SubscribeNewsletterColumn = tw(
-  Column
-)`text-center lg:text-left w-full! lg:w-auto! mt-20 lg:mt-12`;
-const SubscribeNewsletterContainer = tw.div`max-w-sm mx-auto lg:mx-0 `;
-const SubscribeText = tw.p`mt-2 lg:mt-6 text-sm font-medium text-gray-600`;
-const SubscribeForm = tw.form`mt-4 lg:mt-6 text-sm sm:flex max-w-xs sm:max-w-none mx-auto sm:mx-0`;
-const Input = tw.input`bg-gray-300 px-6 py-3 rounded sm:rounded-r-none border-2 sm:border-r-0 border-gray-400 hover:border-primary-500 focus:outline-none transition duration-300 w-full`;
-const SubscribeButton = tw(
-  PrimaryButtonBase
-)`mt-4 sm:mt-0 w-full sm:w-auto rounded sm:rounded-l-none px-8 py-3`;
 
 const Divider = tw.div`my-16 border-b-2 border-gray-300 w-full`;
 
